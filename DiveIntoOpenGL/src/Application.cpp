80c85180f8f8c444381a24ac9cba2d454ffb1e51
Ebrahim Karimi <karimi.gamedev@gmail.com>
2023-12-17 22:56:23 +0330

    Draw a TRIANGLE

--- DiveIntoOpenGL/src/Application.cpp
+++ DiveIntoOpenGL/src/Application.cpp
@@ -19,11 +19,27 @@ int main(void)
 	/* Make the window's context current */
 	glfwMakeContextCurrent(window);
 
+	float posVar = 0.0f;
+	float x1 = -0.5f;
+	float y1 = -0.5f;
+	float x2 = 0.0f;
+	float y2 = 0.5f;
+	float x3 = 0.5f;
+	float y3 = -0.5f;
+
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
 	{
 		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		glBegin(GL_TRIANGLES);
+		glVertex2f(posVar  + x1 , posVar + y1);
+		glVertex2f(posVar + x2, posVar + y2);
+		glVertex2f(posVar + x3, posVar + y3);
+		glEnd();
+
+		//posVar += 0.001f;
 
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
